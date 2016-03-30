102a2f77710b338b85bb5703ad8f09886b2139b5
Homma Takuma <sfprhythnn@gmail.com>
2016-03-30 21:15:50 +0900

    add Tex and iTerm2

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 ## Installation
 
-<<<<<<< HEAD
   git clone git://github.com/rhythnn/dotfiles.git  
   dotfiles/install.sh
-=======
     git clone git://github.com/rhythnn/dotfiles.git  
     dotfiles/install.sh
->>>>>>> origin/master
--- tex_embedHiragino.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo tlmgr update --self --all
-cd /usr/local/texlive/2015basic/texmf-dist/scripts/cjk-gs/integrate
-sudo perl cjk-gs-integrate.pl --link-texmf --force
-sudo mktexlsr
-sudo updmap-sys --setoption kanjiEmbed hiragino-elcapitan
