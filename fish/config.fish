faf8f6e5c1b791cbce687639e29b3de5f0ba4c7a
Homma Takuma <sfprhythnn@gmail.com>
2017-04-23 22:20:33 +0900

    enable C-r search with peco

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,13 @@ set fish_function_path $fish_function_path "/usr/local/lib/python3.6/site-packag
 powerline-setup
 powerline-daemon -q
 
+set current_branch (command git rev-parse --abbrev-ref HEAD ^/dev/null)
+
 # defined in ${HOME}/.config/fish/functions
 set_alias
 set_path
+
+# peco history (Ctrl + r)
+function fish_user_key_bindings
+  bind \cr peco_select_history
+end
