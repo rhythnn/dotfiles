6049a9690b8f3509aabbd50d025b5596703a6541
Homma Takuma <sfprhythnn@gmail.com>
2017-04-23 21:31:43 +0900

    add config.fish

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+set fish_function_path $fish_function_path "/usr/local/lib/python3.6/site-packages/powerline/bindings/fish"
+powerline-setup
+powerline-daemon -q
+
+# defined in ${HOME}/.config/fish/functions
+set_alias
+set_path
