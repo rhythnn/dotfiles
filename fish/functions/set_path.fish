d9a712afa60a1a0bb16c0571bf94502d6db7e507
Homma Takuma <sfprhythnn@gmail.com>
2017-04-24 20:31:06 +0900

    git の editor 設定を gitconfig へ移す

--- fish/functions/set_path.fish
+++ fish/functions/set_path.fish
@@ -4,7 +4,6 @@ function set_path
   set -x PATH $HOME/sl $PATH
   set -x PATH /usr/local/bin $PATH
   set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
-  set -Ux EDITOR "emacsclient -nw"
   set -x PATH $HOME/.cargo/bin $PATH
   set -x PATH $HOME/Library/Android/sdk/tools $PATH
 end
