6a72f163bdd48e2ba08df3f2fcb82912f33a672b
Homma Takuma <sfprhythnn@gmail.com>
2017-04-25 01:12:02 +0900

    alias と env の読み込み元変更

--- fish/config.fish
+++ fish/config.fish
@@ -4,9 +4,8 @@ powerline-daemon -q
 
 set current_branch (command git rev-parse --abbrev-ref HEAD ^/dev/null)
 
-# defined in ${HOME}/.config/fish/functions
-set_alias
-set_path
+. ~/.config/fish/aliases.fish
+. ~/.config/fish/env.fish
 
 # peco history (Ctrl + r)
 function fish_user_key_bindings
