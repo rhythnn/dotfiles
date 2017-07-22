fbbc487cbbd2527dbf6a2da1967958decab88397
Homma Takuma <nagomimatcha@gmail.com>
2017-07-23 02:58:40 +0900

    show emoji on my prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,14 @@
 function fish_prompt
-  set prompt_face '[$] '
-  set_color FD8724
+  if [ $status -eq 0 ]
+    # whale
+    set prompt_face \U1F433
+  else
+    # thinking face
+    set prompt_face \U1F914
+  end
+
+  # set_color FD8724
   echo -n $prompt_face
+  echo -n '  '
   set_color normal
 end
