c1a80b189049d30d139d7915ce91150a800eb61b
Homma Takuma <nagomimatcha@gmail.com>
2017-10-08 00:31:52 +0900

    indent~

--- fish/functions/gco.fish
+++ fish/functions/gco.fish
@@ -1,11 +1,11 @@
 function gco
-	git branch -a | peco | tr -d ' ' | read branch
+  git branch -a | peco | tr -d ' ' | read branch
   if [ $branch ]
-  	if contains $branch "remotes/"
-    	set -l b (echo $branch | awk -F'/' '{print $3}')
-     	git checkout -b $b $branch
+    if contains $branch "remotes/"
+      set -l b (echo $branch | awk -F'/' '{print $3}')
+      git checkout -b $b $branch
     else
-    	git checkout $branch
+      git checkout $branch
     end
   end
   commandline -f repaint
