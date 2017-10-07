54c18d0adafe94ea20e3a8f82a7fd477ff35ea2c
Homma Takuma <nagomimatcha@gmail.com>
2017-10-07 23:41:58 +0900

    set branch if under git controll

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,6 +1,8 @@
 function fish_right_prompt
   if not [ $TMUX ]
     set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
-    echo -ne \ue0a0 $branch
+    if [ $branch ]
+      echo -ne \ue0a0 $branch
+    end
   end
 end
