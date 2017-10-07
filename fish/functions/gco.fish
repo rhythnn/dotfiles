0bd4fc0beff699e8a2ab2f128a54cb6f6f3bf441
Homma Takuma <nagomimatcha@gmail.com>
2017-10-08 00:20:44 +0900

    git checkout with peco

---
+++ fish/functions/gco.fish
@@ -0,0 +1,13 @@
+function gco
+	git branch -a | peco | tr -d ' ' | read branch
+  if [ $branch ]
+  	if contains $branch "remotes/"
+    	set -l b (echo $branch | awk -F'/' '{print $3}')
+     	git checkout -b $b $branch
+    else
+    	git checkout $branch
+    end
+  end
+  commandline -f repaint
+end
+
