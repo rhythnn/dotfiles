7714f75d1fedb957b89610c9622ed9093ae53259
Homma Takuma <nagomimatcha@gmail.com>
2017-10-27 22:45:50 +0900

    should use string match

--- fish/functions/gco.fish
+++ fish/functions/gco.fish
@@ -1,9 +1,9 @@
 function gco
   git branch -a | peco | tr -d ' ' | read branch
   if [ $branch ]
-    if contains $branch "remotes/"
-      set -l b (echo $branch | awk -F'/' '{print $3}')
-      git checkout -b $b $branch
+    if string match "remotes/*" $branch
+      set -l b (echo $branch | awk -F '/' '{print $3 "/" $4}')
+      git checkout $b
     else
       git checkout $branch
     end
