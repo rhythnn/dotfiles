c3e3c1ea861b9a03b526ac3d0a252f4d838fa90b
Takuma Homma <nagomimatcha@gmail.com>
2018-03-04 17:29:23 +0900

    git-commit and my-packages

---
+++ emacs.d/my-packages.el
@@ -0,0 +1,29 @@
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; パッケージ情報の更新
+(package-refresh-contents)
+
+;; インストールするパッケージ
+(defvar my/favorite-packages
+  '(
+    bind-key dash drag-stuff exec-path-from-shell
+
+    expand-region f flycheck htmlize idle-highlight-mode
+
+    multiple-cursors nyan-mode pallet popwin prodigy projectile
+
+    s smartparens smex web-mode yasnippet fish-mode company-emoji
+
+    gitignore-mode gitattributes-mode gitconfig-mode
+    
+    powerline ruby-end nlinum markdown-mode kotlin-mode slim-mode
+
+    robe ruby-mode neotree yaml-mode
+    ))
+
+(dolist (package my/favorite-packages)
+  (unless (package-installed-p package)
+    (package-install package)))
