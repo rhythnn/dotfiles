d4a5d2b8a30b995c15561e62bd9ffefdbad1df32
Takuma Homma <nagomimatcha@gmail.com>
2018-03-17 20:15:39 +0900

    android and homebrew ebv

--- fish/env.fish
+++ fish/env.fish
@@ -1,7 +1,10 @@
-set -x HOMEBREW_CASK_OPTS "--appdir=/Applications --caskroom=/usr/local/Caskroom"
+set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
 set -x PATH /usr/local/bin $PATH
 set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
 set -x PATH $HOME/Library/Android/sdk/tools $PATH
 set -x GOPATH $HOME/go
 set -x EDITOR "emacsclient -nw -t"
 set -x PATH $HOME/Library/Android/sdk/platform-tools $PATH
+set -x PATH $HOME/Library/Android/sdk/tools/bin $PATH
+set fish_greeting ""
+set -x HOMEBREW_NO_AUTO_UPDATE 1
