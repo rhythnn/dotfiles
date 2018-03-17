68627ff32f2783710bf2df3d76ed87fb8dc90e1b
Takuma Homma <nagomimatcha@gmail.com>
2018-03-17 23:49:53 +0900

    with leaf

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -2,6 +2,6 @@ function fish_title
   prompt_pwd
   set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
   if [ $branch ]
-    echo -ne "      " $branch
+    echo -ne "     🌱   branch: " $branch
   end
 end
