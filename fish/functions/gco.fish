666d981e6fa09d02082538f68dcd53847a005eaf
Takuma Homma <nagomimatcha@gmail.com>
2018-07-28 23:50:51 +0900

    Parse remote branch correctly

--- fish/functions/gco.fish
+++ fish/functions/gco.fish
@@ -2,7 +2,7 @@ function gco
   git branch -a | peco | tr -d ' ' | read branch
   if [ $branch ]
     if string match "remotes/*" $branch
-      set -l b (echo $branch | awk -F '/' '{print $3 "/" $4}')
+      set -l b (string replace 'remotes/origin/' '' $branch)
       git checkout $b
     else
       git checkout $branch
