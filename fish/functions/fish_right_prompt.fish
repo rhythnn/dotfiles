c1885c3f8934266801b3626d1f8de6d23c89535a
Takuma Homma <nagomimatcha@gmail.com>
2018-09-09 22:05:01 +0900

    :wave: Clean fish right prompt

--- fish/functions/fish_right_prompt.fish
+++
@@ -1,8 +0,0 @@
-function fish_right_prompt
-  if not [ $TMUX ]
-    set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
-    if [ $branch ]
-      echo -ne \ue0a0 $branch
-    end
-  end
-end
