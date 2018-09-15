84d4149a2ad6030e643575b5512b8a377194ea36
Takuma Homma <nagomimatcha@gmail.com>
2018-09-15 22:56:01 +0900

    No need to use powerline

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
-set fish_function_path $fish_function_path "/usr/local/lib/python3.6/site-packages/powerline/bindings/fish"
-powerline-setup
-powerline-daemon -q
+# set fish_function_path $fish_function_path "/usr/local/lib/python3.6/site-packages/powerline/bindings/fish"
+# powerline-setup
+# powerline-daemon -q
 
 set current_branch (command git rev-parse --abbrev-ref HEAD ^/dev/null)
 
@@ -27,8 +27,8 @@ function fish_user_key_bindings
   bind \cr peco_select_history
 end
 
-if not [ $TMUX ]
-  tmux
-end
+# if not [ $TMUX ]
+  # tmux
+# end
 
 set -x LANG ja_JP.UTF-8
