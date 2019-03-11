9718fcdb7e4c525b8b52e40dc8079677dab95fde
Takuma Homma <nagomimatcha@gmail.com>
2019-03-12 00:07:41 +0900

    Ruby

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -16,7 +16,6 @@
 
 (require 'company)
 (global-company-mode)
-(setq company-selection-wrap-around t)
 
 (require 'company-emoji)
 (add-to-list 'company-backends 'company-emoji)
@@ -129,8 +128,27 @@
 
 ;; robe
 (add-hook 'ruby-mode-hook 'robe-mode)
+(autoload 'robe-mode "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
 (eval-after-load 'company
   '(push 'company-robe company-backends))
+
+(add-hook 'ruby-mode-hook (lambda()
+      (company-mode)
+      (setq company-auto-expand t)
+      (setq company-transformers '(company-sort-by-backend-importance)) ;; ソート順
+      (setq company-idle-delay 0) ; 遅延なし
+      (setq company-minimum-prefix-length 1) ; 何文字打つと補完動作を行うか設定
+      (setq company-selection-wrap-around t) ; 候補の最後の次は先頭に戻る
+      (setq completion-ignore-case t)
+      (setq company-dabbrev-downcase nil)
+      (global-set-key (kbd "C-M-i") 'company-complete)
+      ;; C-n, C-pで補完候補を次/前の候補を選択
+      (define-key company-active-map (kbd "C-n") 'company-select-next)
+      (define-key company-active-map (kbd "C-p") 'company-select-previous)
+      (define-key company-active-map (kbd "C-s") 'company-filter-candidates) ;; C-sで絞り込む
+      (define-key company-active-map [tab] 'company-complete-selection) ;; TABで候補を設定
+      (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
+      ))
 
 ;;; カーソルの位置が何行目かを表示する
 (line-number-mode t)
