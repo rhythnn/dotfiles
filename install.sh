96c3934b0b55c288157ec8dc783eb058785e0263
Takuma Homma <nagomimatcha@gmail.com>
2019-07-27 01:21:46 +0900

    Add xcode-theme based material theme

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ ln -sf ~/src/github.com/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/src/github.com/dotfiles/emacs.d ~/.emacs.d
 ln -sf ~/src/github.com/dotfiles/powerline ~/.config/powerline
 ln -sf ~/src/github.com/dotfiles/fish ~/.config/fish
-ln -sf ~/src/github.com/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/src/github.com/dotfiles/atom/config.cson ~/.atom/config.cson
 ln -sf ~/src/github.com/dotfiles/atom/styles.less ~/.atom/styles.less
 ln -sf ~/src/github.com/dotfiles/.tigrc ~/.tigrc
 ln -sf ~/src/github.com/dotfiles/.irbrc ~/.irbrc
 ln -sf ~/src/github.com/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/src/github.com/dotfiles/xcode-theme/Material\ Dark.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/Material\ Dark.xccolortheme
