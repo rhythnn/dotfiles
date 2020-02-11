cbfbf460a11d4651a7235f6c3e99c5785002d182
Takuma Homma <nagomimatcha@gmail.com>
2020-02-11 23:47:04 +0900

    Simple terminal

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,14 +1,45 @@
+# name: trout
+
+function _git_branch_name
+  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _is_git_dirty
+  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+end
+
+function _file_count
+  ls -1 | wc -l | sed 's/\ //g'
+end
+
 function fish_prompt
-  if [ $status -eq 0 ]
-    # whale
-    set prompt_face \U1F433
-  else
-    # thinking face
-    set prompt_face \U1F914
+  set -l last_status $status
+  set -l yellow (set_color yellow)
+  set -l red (set_color red)
+  set -l green (set_color green)
+  set -l magenta (set_color magenta)
+  set -l normal (set_color normal)
+
+  if [ (_git_branch_name) ]
+    set -l git_branch $magenta(_git_branch_name)
+    set git_info "$git_branch"
+
+    if [ (_is_git_dirty) ]
+      set git_info "$git_info$yellow*"
+    end
+
+    set git_info "$magenta($git_info$magenta)$normal"
   end
 
-  # set_color FD8724
-  echo -n $prompt_face
-  echo -n '  '
-  set_color normal
+  if test $last_status = 0
+    set prompt " $green⫸ $normal "
+  else
+    set prompt " $red⫸ $normal "
+  end
+
+  echo -n -s $git_info $prompt
+end
+
+function fish_right_prompt
+  echo ''
 end
