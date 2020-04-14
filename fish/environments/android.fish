7bfb0e80779b7a81ab8558b893d568a041b5a6e7
Takuma Homma <nagomimatcha@gmail.com>
2020-04-14 18:26:46 +0900

    gradle and bundletool

--- fish/environments/android.fish
+++ fish/environments/android.fish
@@ -6,3 +6,5 @@ set -x PATH $HOME/Library/Android/sdk/emulator $PATH
 set -x PATH $HOME/Library/Android/sdk/cmdline-tools/latest/bin $PATH
 set -x ANDROID_HOME $HOME/Library/Android/sdk
 set -x PATH $HOME/Library/Android/sdk/build-tools/29.0.3 $PATH
+alias bundletool 'java -jar ~/bundletool-all-0.13.3.jar'
+alias clean './gradlew clean'
