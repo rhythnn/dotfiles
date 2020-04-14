5a4ac89ef45a6f09dd45e2b093bd3b570f67a496
Takuma Homma <nagomimatcha@gmail.com>
2020-04-14 18:22:19 +0900

    Define android specific path in android.fish

--- fish/env.fish
+++ fish/env.fish
@@ -1,17 +1,14 @@
 set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
 set -x PATH /usr/local/bin $PATH
-set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
-set -x PATH $HOME/Library/Android/sdk/tools $PATH
 set -x GOPATH $HOME/go
 set -x EDITOR "emacsclient -nw -t"
-set -x PATH $HOME/Library/Android/sdk/platform-tools $PATH
-set -x PATH $HOME/Library/Android/sdk/tools/bin $PATH
 set fish_greeting ""
 set -x HOMEBREW_NO_AUTO_UPDATE 1
-set -x ANDROID_HOME $HOME/Library/Android/sdk
-set -x PATH $HOME/Library/Android/sdk/build-tools/28.0.2 $PATH
+set -x PATH $HOME/src/github.com/flutter/flutter/bin $PATH
 set -x LANG ja_JP.UTF-8
 # https://fishshell.com/docs/current/index.html#variables-color
 set fish_color_command normal
 # https://www.materialui.co/colors
 set fish_color_error FF7043
+
+. ~/.config/fish/environments/android.fish
---
+++ fish/environments/android.fish
@@ -0,0 +1,8 @@
+set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+set -x PATH $HOME/Library/Android/sdk/tools $PATH
+set -x PATH $HOME/Library/Android/sdk/platform-tools $PATH
+set -x PATH $HOME/Library/Android/sdk/tools/bin $PATH
+set -x PATH $HOME/Library/Android/sdk/emulator $PATH
+set -x PATH $HOME/Library/Android/sdk/cmdline-tools/latest/bin $PATH
+set -x ANDROID_HOME $HOME/Library/Android/sdk
+set -x PATH $HOME/Library/Android/sdk/build-tools/29.0.3 $PATH
