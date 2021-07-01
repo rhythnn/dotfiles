4d91f84c948a1c6b7c67a2f9341cdaae08d26dd1
Takuma Homma <nagomimatcha@gmail.com>
2021-07-01 23:11:19 +0900

    Enable coldboot

--- fish/functions/emu.fish
+++ fish/functions/emu.fish
@@ -1,4 +1,11 @@
 function emu
-  avdmanager list avd | grep 'Name:' | awk -F ':' '{print $2}' | fzf | xargs -I{} emulator @{}
+  if [ $argv[1] ] 
+    if test $argv[1] = '--coldboot'
+      set option '-no-snapshot-load'
+    end
+  end
+
+  avdmanager list avd | grep 'Name:' | awk -F ':' '{print $2}' | fzf | xargs -I{} emulator @{} $option
+  set -e option
 end
 
